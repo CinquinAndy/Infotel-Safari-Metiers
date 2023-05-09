3693ab33ab2e8f09bf6e80718e4037d509b5cf5a
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-09 11:01:16 +0200

    init

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+import React from 'react'
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Safari des métiers</title>
+				<meta name="description" content="Safari des métiers" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div>
+				index
+			</div>
+		</>
+	)
+}
