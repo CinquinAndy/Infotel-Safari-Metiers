505302d900ff01be9d517dcdcb5b66eb1f6539ad
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-09 13:00:10 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	webpack: config => {
-		config.plugins.push(
-			new CopyPlugin({
-				patterns: [
-					{
-						from: 'node_modules/leaflet/dist/images',
-						to: path.resolve(__dirname, 'public', 'leaflet', 'images'),
-					},
-				],
-			})
-		)
-		return config
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**.andy-cinquin.fr',
+			},
+		],
 	},
 }
 
