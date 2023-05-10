1f700953dd25ee1f407a9a672016c3a41a8b3866
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-10 16:21:47 +0200

    albums items

--- src/pages/albums.js
+++ src/pages/albums.js
@@ -3,110 +3,114 @@ import Head from 'next/head'
 import React from 'react'
 import Image from 'next/image'
 import Nav from '@components/Global/nav'
-import PocketBase from 'pocketbase'
-import { useQuery } from '@tanstack/react-query'
+import {useQuery} from '@tanstack/react-query'
 import Loader from '@components/Global/Loader'
+import Router from "next/router";
+import {initPocketBase} from "../../lib/pocketbase";
 
-export default function Albums() {
-	const pb = new PocketBase('http://infotel-safari-api.beta.andy-cinquin.fr')
+export default function Albums(props) {
+    const pb = initPocketBase()
 
-	const { isLoading, isError, data, error } = useQuery({
-		queryKey: ['albums'],
-		queryFn: async () => {
-			return pb.collection('albums').getFullList({
-				sort: '-created',
-			})
-		},
-	})
+    const {isLoading, isError, data, error} = useQuery({
+        queryKey: ['albums'],
+        queryFn: async () => {
+            return pb.collection('albums').getFullList({
+                sort: '-created',
+            })
+        },
+    })
 
-	console.log(isLoading, isError, data, error)
+    console.log(isLoading, isError, data, error)
 
-	if (isLoading) return <Loader />
+    if (isLoading) return <Loader/>
 
-	if (error) return 'An error has occurred: ' + error.message
-	const albums = data
+    if (error) return 'An error has occurred: ' + error.message
+    const albums = data
 
-	console.log(albums)
+    console.log(albums)
 
-	return (
-		<>
-			<Head>
-				<title>Safari des métiers</title>
-				<meta name="description" content="Safari des métiers" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+    return (
+        <>
+            <Head>
+                <title>Safari des métiers</title>
+                <meta name="description" content="Safari des métiers"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-			<>
-				<Nav />
-				<div className={'relative h-screen w-screen'}>
-					<main
-						className={'mx-auto h-screen min-h-screen max-w-7xl px-8 pt-28'}
-					>
-						<section className={'flex w-full items-center justify-start'}>
-							<div
-								className={
-									'flex h-full w-1/2 flex-col items-start justify-start'
-								}
-							>
-								<h1 className={'flex flex-col gap-2'}>
+            <>
+                <Nav/>
+                <div className={'relative min-h-screen w-screen'}>
+                    <main
+                        className={'mx-auto h-screen min-h-screen max-w-7xl px-8 py-28 '}
+                    >
+                        <section className={'flex w-full items-center justify-start'}>
+                            <div
+                                className={
+                                    'flex h-full w-1/2 flex-col items-start justify-start'
+                                }
+                            >
+                                <h1 className={'flex flex-col gap-2'}>
 									<span className={'text-2xl text-teal-500 sm:text-5xl'}>
-										Mes albums !
+										Mes albums&nbsp;!
 									</span>
-								</h1>
-							</div>
-						</section>
-						<ul
-							role="list"
-							className="mt-8 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
-						>
-							{albums.map(album => (
-								<li key={album.id} className="relative">
-									<div
-										className={
-											'group aspect-h-10 aspect-w-10 block w-full overflow-hidden rounded-lg bg-slate-100 transition hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 hover:ring-offset-slate-100'
-										}
-									>
-										<Image
-											src={`http://infotel-safari-api.beta.andy-cinquin.fr/api/files/${album.collectionId}/${album.id}/${album.photos[0]}`}
-											alt="Photo Album"
-											width={500}
-											height={500}
-											className="pointer-events-none object-cover object-center group-hover:opacity-75"
-										/>
-										<button
-											type="button"
-											className="absolute inset-0 focus:outline-none"
-										>
+                                </h1>
+                            </div>
+                        </section>
+                        <ul
+                            role="list"
+                            className="my-8 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8 pb-20"
+                        >
+                            {albums.map(album => (
+                                <li key={album.id} className="relative">
+                                    <div
+                                        className={
+                                            'group aspect-h-10 aspect-w-10 block w-full overflow-hidden rounded-lg bg-slate-100 transition hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 hover:ring-offset-slate-100'
+                                        }
+                                    >
+                                        <Image
+                                            src={`http://infotel-safari-api.beta.andy-cinquin.fr/api/files/${album.collectionId}/${album.id}/${album.photos[0]}`}
+                                            alt="Photo Album"
+                                            width={500}
+                                            height={500}
+                                            className="pointer-events-none object-cover object-center group-hover:opacity-75 group-hover:scale-105 transition-all duration-300"
+                                        />
+                                        <button
+                                            type="button"
+                                            className="absolute inset-0 focus:outline-none"
+                                            onClick={() => {
+                                                Router.push(`/albums/${album.id}`)
+                                            }}
+                                        >
 											<span className="sr-only">
 												View details for {album.title}
 											</span>
-										</button>
-									</div>
-									<p className="pointer-events-none mt-2 block truncate text-sm font-medium text-slate-900">
-										{album.title}
-									</p>
-								</li>
-							))}
-						</ul>
-						{/*<section className={'grid-custom'}>*/}
-						{/*	{albums[4].photos.map(photo => {*/}
-						{/*		return (*/}
-						{/*			<div className={'relative h-[300px] w-[300px]'} key={photo}>*/}
-						{/*				<Image*/}
-						{/*					alt={'Albums'}*/}
-						{/*					src={`http://infotel-safari-api.beta.andy-cinquin.fr/api/files/${albums[4].collectionId}/${albums[4].id}/${photo}`}*/}
-						{/*					quality={75}*/}
-						{/*					width={500}*/}
-						{/*					height={500}*/}
-						{/*					className={'h-full w-full object-cover object-center'}*/}
-						{/*				/>*/}
-						{/*			</div>*/}
-						{/*		)*/}
-						{/*	})}*/}
-						{/*</section>*/}
-					</main>
-				</div>
-			</>
-		</>
-	)
+                                        </button>
+                                    </div>
+                                    <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-slate-900">
+                                        {album.title}
+                                    </p>
+                                </li>
+                            ))}
+                        </ul>
+                        {/*<section className={'grid-custom'}>*/}
+                        {/*	{albums[4].photos.map(photo => {*/}
+                        {/*		return (*/}
+                        {/*			<div className={'relative h-[300px] w-[300px]'} key={photo}>*/}
+                        {/*				<Image*/}
+                        {/*					alt={'Albums'}*/}
+                        {/*					src={`http://infotel-safari-api.beta.andy-cinquin.fr/api/files/${albums[4].collectionId}/${albums[4].id}/${photo}`}*/}
+                        {/*					quality={75}*/}
+                        {/*					width={500}*/}
+                        {/*					height={500}*/}
+                        {/*					className={'h-full w-full object-cover object-center'}*/}
+                        {/*				/>*/}
+                        {/*			</div>*/}
+                        {/*		)*/}
+                        {/*	})}*/}
+                        {/*</section>*/}
+                    </main>
+                </div>
+            </>
+        </>
+    )
 }
--- src/pages/albums/[id].js
+++ src/pages/albums/[id].js
@@ -1,25 +1,51 @@
-export default function Album({ albumData }) {
-	return (
-		<>
-			<div>
-				<h1>Album</h1>
-				<br />
-				<p>albumData : {JSON.stringify(albumData)}</p>
-			</div>
-		</>
-	)
+import PocketBase from "pocketbase";
+import {getAlbumIdList, getAlbumsDetails} from "../../../lib/albums";
+import {useQuery} from "@tanstack/react-query";
+import {initPocketBase} from "../../../lib/pocketbase";
+
+
+export default function Album({albumData}) {
+    return (
+        <>
+            <div>
+                <h1>Album</h1>
+                <br/>
+                <p>albumData : {JSON.stringify(albumData)}</p>
+            </div>
+        </>
+    )
 }
 
 export async function getStaticPaths() {
-	// Return a list of possible value for id
+    const pb = initPocketBase()
+    const res = await pb.collection('albums_ids').getFullList()
+    /**
+     * format the data for getStaticPaths
+     * @type {{params: {id: *}}[]}
+     */
+    const paths = res.map(record => ({
+        params: {
+            id: record.id
+        }
+    }));
+    return {
+        paths,
+        fallback: false
+    }
 }
 
-export async function getStaticProps({ params }) {
-	// Fetch necessary data for the blog post using params.id
-	const albumData = { id: params.id }
-	return {
-		props: {
-			albumData,
-		},
-	}
+export async function getStaticProps({params}) {
+    const pb = initPocketBase()
+    const record = await pb.collection('albums').getOne(params.id)
+    const albumData = await getAlbumsDetails(params.id)
+    if (!albumData) {
+        return {
+            props: {hasError: true},
+        }
+    }
+    return {
+        props: {
+            albumData
+        },
+    }
 }
