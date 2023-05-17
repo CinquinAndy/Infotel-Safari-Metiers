8d5681abf821f04bcf45a28581a13d4ce1fb39bd
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-17 15:44:03 +0200

    tailwindcss problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,10 @@
 module.exports = {
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/pages/*.{js,ts,jsx,tsx}',
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 		'./src/app/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {},
-	plugins: [
-		// ...
-		require('@tailwindcss/aspect-ratio'),
-	],
+	plugins: [require('@tailwindcss/aspect-ratio')],
 }
